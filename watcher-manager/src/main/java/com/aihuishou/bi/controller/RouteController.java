d6dbaa3bcf0364ffdc582f06cc1c63fb03084374
yule.zhang <yule.zhang@aihuishou.com>
2019-07-12 14:06:06 +0800

    打印可疑代码日志

--- watcher-manager/src/main/java/com/aihuishou/bi/controller/RouteController.java
+++ watcher-manager/src/main/java/com/aihuishou/bi/controller/RouteController.java
@@ -3,6 +3,8 @@ package com.aihuishou.bi.controller;
 import com.aihuishou.bi.core.SysConf;
 import com.aihuishou.bi.service.SysService;
 import com.google.common.collect.ImmutableMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -17,6 +19,8 @@ import java.util.Map;
 @RequestMapping(value = "/route")
 public class RouteController {
 
+    private final Logger logger = LoggerFactory.getLogger(RouteController.class);
+
     @Autowired
     private SysService sysService;
 
@@ -26,6 +30,8 @@ public class RouteController {
         Map<String, String> positionMap = sysService.getPositionMap();
         String loadName = positionMap.get(position) + SysConf.REST_JS_SUFFIX;
         String staticName = positionMap.get(position) + SysConf.REST_HTML_SUFFIX;
+        logger.info(loadName);
+        logger.info(staticName);
         model.addAttribute("model" , ImmutableMap.of("position", position, "loadName", loadName, "staticName", staticName));
         return "base";
     }
