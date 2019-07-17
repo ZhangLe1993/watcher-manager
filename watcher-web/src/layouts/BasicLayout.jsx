178b335394053b9da691bf1deb4cbd5c53e9180b
yangang.li <yangang.li@aihuishou.com>
2019-07-17 16:14:15 +0800

    url&copyRight

--- watcher-web/src/layouts/BasicLayout.jsx
+++ watcher-web/src/layouts/BasicLayout.jsx
@@ -4,14 +4,22 @@
  * https://github.com/ant-design/ant-design-pro-layout
  */
 import ProLayout from '@ant-design/pro-layout';
-import React, { useEffect, useState } from 'react';
+import React,
+{
+  useEffect,
+  // useState,
+  Fragment,
+} from 'react';
+import { Icon, Layout } from 'antd';
 import Link from 'umi/link';
 import { connect } from 'dva';
 import { formatMessage } from 'umi-plugin-react/locale';
 import Authorized from '@/utils/Authorized';
 import RightContent from '@/components/GlobalHeader/RightContent';
-import { isAntDesignPro } from '@/utils/utils';
+import GlobalFooter from '@/components/GlobalFooter/index';
+// import { isAntDesignPro } from '@/utils/utils';
 import logo from '../assets/logo.svg';
+
 // import { queryPageView } from '../services/menu';
 
 /**
@@ -25,30 +33,19 @@ const menuDataRender = menuList =>
     const localItem = { ...item, children: item.children ? menuDataRender(item.children) : [] };
     return Authorized.check(item.authority, localItem, null);
   });
+const { Footer } = Layout;
 
-const footerRender = (_, defaultDom) => {
-  if (!isAntDesignPro()) {
-    return defaultDom;
-  }
-
+const footerRender = () => {
   return (
-    <>
-      {defaultDom}
-      <div
-        style={{
-          padding: '0px 24px 24px',
-          textAlign: 'center',
-        }}
-      >
-        <a href="https://www.netlify.com" target="_blank" rel="noopener noreferrer">
-          <img
-            src="https://www.netlify.com/img/global/badges/netlify-color-bg.svg"
-            width="82px"
-            alt="netlify logo"
-          />
-        </a>
-      </div>
-    </>
+    <Footer style={{ padding: 0 }}>
+      <GlobalFooter
+        copyright={
+          <Fragment>
+            Copyright <Icon type="copyright" /> 2019 爱回收-数据部 出品
+          </Fragment>
+        }
+      />
+  </Footer>
   );
 };
 
@@ -101,7 +98,7 @@ const BasicLayout = props => {
   };
 
   const myclick = (e, it) => {
-    console.log(it, '-it-');
+    // console.log(it, '-it-');
     const { component, path } = it;
     const flagArr = path.split(`${component}/`);
     let flag = '';
@@ -117,15 +114,15 @@ const BasicLayout = props => {
     <ProLayout
       logo={logo}
       onCollapse={handleMenuCollapse}
-      menuItemRender = { (menuItemProps, dom) => {
-        return <div onClick={e => myclick(e, menuItemProps)}>{dom}</div>;
-      }}
-      // menuItemRender={(menuItemProps, defaultDom) => {
-      //   if (menuItemProps.isUrl) {
-      //     return defaultDom;
-      //   }
-      //   return <Link to={`/watcher/${menuItemProps.component}`}>{defaultDom}</Link>;
+      // menuItemRender = { (menuItemProps, dom) => {
+      //   return <div onClick={e => myclick(e, menuItemProps)}>{dom}</div>;
       // }}
+      menuItemRender={(menuItemProps, defaultDom) => {
+        if (menuItemProps.isUrl) {
+          return defaultDom;
+        }
+        return <Link to={`/watcher/${menuItemProps.component}`}>{defaultDom}</Link>;
+      }}
       breadcrumbRender={(routers = []) => [
         {
           path: '/',
--- watcher-web/src/pages/Iframe/index.jsx
+++ watcher-web/src/pages/Iframe/index.jsx
@@ -1,17 +1,46 @@
 import React from 'react';
+// import { Spin } from 'antd';
+
+// import style from './index.less';
 
 class Iframe extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      // loading: true,
     };
+  }
+
+  componentDidMount() {
+    const position = this.props.location.pathname.split('/watcher/')[1];
+    const watcherIframe = document.getElementById('watcherIframe');
+    watcherIframe.src = `/route/base?position=${position}`;
+  }
+
+
+  componentWillReceiveProps(nextProps) {
+    const prePosition = this.props.location.pathname.split('/watcher/')[1];
+    const nextPosition = nextProps.location.pathname.split('/watcher/')[1];
+    if (prePosition !== nextPosition) {
+      const watcherIframe = document.getElementById('watcherIframe');
+      watcherIframe.src = `/route/base?position=${nextPosition}`;
+    }
+  }
+
+  handleIframe = () => {
+    // this.setState({ loading: false });
   }
 
   render() {
     return (
-      <div>
-        <iframe id="watcherIframe" title="watcher" scrolling="no" frameBorder="0" width="100%" height="100%">{/* 占位 */}</iframe>
+      <div style={{ height: '100%' }}>
+      {
+        /*
+         <Spin delay={200} size="large" spinning={this.state.loading}>
+         </Spin>
+        */
+      }
+      <iframe id="watcherIframe" title="watcher" scrolling="auto" frameBorder="0" width="100%" height="100%" minHeight="1000px">{/* 占位 */}iframe</iframe>
       </div>
     );
   }
