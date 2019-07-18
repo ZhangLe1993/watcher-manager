76c700082b34c5dd63ffd4806c5a671ddf63dc92
yule.zhang <yule.zhang@aihuishou.com>
2019-07-18 16:14:36 +0800

    加入缓存

--- watcher-manager/src/main/java/com/aihuishou/bi/service/SysService.java
+++ watcher-manager/src/main/java/com/aihuishou/bi/service/SysService.java
@@ -6,6 +6,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ResourceUtils;
 
@@ -34,7 +35,7 @@ public class SysService {
     }*/
 
 
-//    @Cacheable(value = "position-map", keyGenerator = "watcherManagerKeyGenerator")
+    @Cacheable(value = "position-map", keyGenerator = "watcherManagerKeyGenerator")
     public Map<String, String> getPositionMap() {
         String target = path + SysConf.OLD_FILE_FOLDER;
         Set<String> files = ScanFolder.traverseFolder(target);
