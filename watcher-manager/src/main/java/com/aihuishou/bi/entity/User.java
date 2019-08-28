0b0e782f2b429211bcfa5e1fa967114380326151
yule.zhang <yule.zhang@aihuishou.com>
2019-08-28 20:10:05 +0800

    CurrentUser是否是管理员

--- watcher-manager/src/main/java/com/aihuishou/bi/entity/User.java
+++ watcher-manager/src/main/java/com/aihuishou/bi/entity/User.java
@@ -19,4 +19,6 @@ public class User implements Serializable {
 
     private String employeeNo;
 
+    private Boolean admin = false;
+
 }
--- watcher-manager/src/main/java/com/aihuishou/bi/service/UserService.java
+++ watcher-manager/src/main/java/com/aihuishou/bi/service/UserService.java
@@ -1,5 +1,6 @@
 package com.aihuishou.bi.service;
 
+import com.aihuishou.bi.core.Admin;
 import com.aihuishou.bi.entity.Role;
 import com.aihuishou.bi.entity.User;
 import org.apache.commons.dbutils.QueryRunner;
@@ -7,19 +8,15 @@ import org.apache.commons.dbutils.handlers.BeanHandler;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
 import org.apache.commons.dbutils.handlers.ScalarHandler;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import javax.sql.DataSource;
-import javax.xml.crypto.Data;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @Service
 public class UserService {
@@ -31,10 +28,18 @@ public class UserService {
     @Resource
     private DataSource dataSource;
 
+    @Autowired
+    private AdminService adminService;
+
     @Cacheable(value = "current-user", key = "#obId")
     public User getUserByObId(String obId) throws SQLException {
         String sql = "SELECT observer_id as obId, observer_name AS name, observer_mobile AS mobile,observer_account_name AS email,coalesce(employee_no,observer_employee_no) AS employeeNo from dim.dim_observer WHERE observer_id=? order by employee_no desc limit 1;";
-        return new QueryRunner(greenPlum).query(sql, new BeanHandler<>(User.class), Long.parseLong(obId));
+        User user = new QueryRunner(greenPlum).query(sql, new BeanHandler<>(User.class), Long.parseLong(obId));
+        Admin admin = adminService.inBoss(user.getEmployeeNo());
+        if(admin.getCode() != Admin.GUEST.getCode()) {
+            user.setAdmin(true);
+        }
+        return user;
     }
 
     public List<User> all(String key, int pageIndex, int pageSize) throws SQLException {
