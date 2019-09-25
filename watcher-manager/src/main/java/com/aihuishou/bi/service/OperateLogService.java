5c79b86bfb02243ecfeca813a13c1faae3113831
111467 <huhao418917>
2019-09-25 10:46:22 +0800

    添加节点操作记录读取接口

--- watcher-manager/src/main/java/com/aihuishou/bi/service/OperateLogService.java
+++ watcher-manager/src/main/java/com/aihuishou/bi/service/OperateLogService.java
@@ -1,12 +1,18 @@
 package com.aihuishou.bi.service;
 
 import com.aihuishou.bi.entity.OperateLog;
+import com.aihuishou.bi.vo.OperationInfo;
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.ResultSetHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import javax.sql.DataSource;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class OperateLogService {
@@ -22,5 +28,14 @@ public class OperateLogService {
     public void logger(OperateLog operateLog) throws SQLException {
         String sql = "insert into operate_log_b (event, target, target_id, operate_type, description, employee_name, employee_no, operate_time) values (?, ?, ?, ?, ?, ?, ?, ?);";
         new QueryRunner(dataSource).update(sql, operateLog.getEvent(), operateLog.getTarget(), operateLog.getTargetId(), operateLog.getOperateType(), operateLog.getDescription(), operateLog.getEmployeeName(), operateLog.getEmployeeNo(), operateLog.getOperateTime());
+    }
+
+    public List<OperationInfo> getOperationInfoList(String nodeType, Integer nodeId) throws SQLException {
+        String sql = "select event, description,employee_name, operate_time from operate_log_b where target=? and target_id=?";
+        List<List<OperationInfo>> operationInfos = new QueryRunner(dataSource).execute(sql, new BeanListHandler<OperationInfo>(OperationInfo.class), nodeType, nodeId);
+        if(operationInfos != null && operationInfos.size() > 0){
+            return operationInfos.get(0);
+        }
+        return new ArrayList<OperationInfo>();
     }
 }
