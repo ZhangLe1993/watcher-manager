6163b063c9fb108fa8a9e9ea1a2fb814d76e60fe
yangang.li <yangang.li@aihuishou.com>
2019-11-12 17:47:31 +0800

    bug fixs

--- watcher-web/src/models/global.js
+++ watcher-web/src/models/global.js
@@ -109,6 +109,19 @@ const GlobalModel = {
     setup({ history }) {
       // Subscribe history(url) change, trigger `load` action if pathname is `/`
       history.listen(({ pathname, search }) => {
+        let clickNum = 0;
+        let clickNumMap = {};
+        const fullPathArr = pathname.split('/');
+        const component = fullPathArr[fullPathArr.length - 1];
+        const storeKey = 'click-num-map';
+        if (window.localStorage.getItem(storeKey)) {
+          clickNumMap = JSON.parse(window.localStorage.getItem(storeKey));
+          clickNum = clickNumMap[component] ? clickNumMap[component][0] + 1 : 1;
+        } else {
+          clickNum += 1;
+        }
+        clickNumMap[component] = [clickNum, pathname];
+        window.localStorage.setItem(storeKey, JSON.stringify(clickNumMap));
         if (typeof window.ga !== 'undefined') {
           window.ga('send', 'pageview', pathname + search);
         }
