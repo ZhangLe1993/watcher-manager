fb57e222149c4de57959459b980ca17b5ec2c9b3
111150 <chunhui.wang@aihuishou.com>
2019-12-17 17:52:37 +0800

    add davinci路由页

--- watcher-web/config/config.js
+++ watcher-web/config/config.js
@@ -102,6 +102,12 @@ export default {
           component: './Iframe',
         },
         {
+          path: '/davinci/**',
+          name: 'davinci',
+          icon: 'smile',
+          component: './davinci',
+        },
+        {
           path: 'pages/watcher_manager',
           name: 'manager',
           icon: 'smile',
