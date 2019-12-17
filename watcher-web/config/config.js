88e140d584071cb5a160f54bc14f53a4c54f268d
111150 <chunhui.wang@aihuishou.com>
2019-12-17 21:46:42 +0800

    fixed

--- watcher-web/config/config.js
+++ watcher-web/config/config.js
@@ -105,7 +105,7 @@ export default {
           path: '/davinci/**',
           name: 'davinci',
           icon: 'smile',
-          component: './davinci',
+          component: './Davinci',
         },
         {
           path: 'pages/watcher_manager',
